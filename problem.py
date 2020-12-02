35295a62176481bda70edef2974dd0ced6dc89f1
Marissa Aguilera <marissaguilera@yahoo.com>
2020-12-01 18:22:42 -0800

    test cases

---
+++ problem.py
@@ -0,0 +1,12 @@
+
+
+def pig_latin(string):
+    """Given a string change the text to pig latin."""
+
+print(pig_latin("pig")) #igpay 
+print(pig_latin("egg")) #eggay
+print(pig_latin("Hello")) #Ellohay
+print(pig_latin("Orange")) #Orangeay
+print(pig_latin("Struck")) #Uckstray - stuck here 
+print(pig_latin("Hello world")) #Ellohay orldway - stuck here 
+print(pig_latin("Hello, world.")) #Ellohay, orldway
