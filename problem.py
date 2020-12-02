233bd9d35debe8d7a5f3482e28ab22e383b0ec8f
Marissa Aguilera <marissaguilera@yahoo.com>
2020-12-01 18:52:25 -0800

    for loop

--- problem.py
+++ problem.py
@@ -1,7 +1,18 @@
 
 
+# Write a function that translates english to pig latin.
+# constant at the end 
+# add ay at the end 
+# if it already caps then it stays caps 
+
 def pig_latin(string):
     """Given a string change the text to pig latin."""
+
+    vowels = ['a', 'e', 'i', 'o', 'u'] # need to account for caps also 
+
+    for char in string: 
+        if char in vowels: 
+            print(char)
 
 print(pig_latin("pig")) #igpay 
 print(pig_latin("egg")) #eggay
@@ -9,4 +20,13 @@ print(pig_latin("Hello")) #Ellohay
 print(pig_latin("Orange")) #Orangeay
 print(pig_latin("Struck")) #Uckstray - stuck here 
 print(pig_latin("Hello world")) #Ellohay orldway - stuck here 
-print(pig_latin("Hello, world.")) #Ellohay, orldway
+print(pig_latin("Hello, world.")) #Ellohay, orldway - stuck here 
+
+
+# all constants before the first vowel get placed at the end 
+# last two letters are "ay"
+
+
+#pseudocode: 
+# for loop through each letter 
+# if statement checking if it is a constant or a vowel 
